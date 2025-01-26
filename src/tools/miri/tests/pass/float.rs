64595c64a14331e25987a3f04c180b2199aca7fe
bendn <bend.n@outlook.com>
2025-01-26 11:10:35 +0700

    Suggest {to,from}_ne_bytes for transmutations between arrays and integers, etc

--- src/tools/miri/tests/pass/float.rs
+++ src/tools/miri/tests/pass/float.rs
@@ -5,7 +5,7 @@
 #![feature(f128)]
 #![feature(f16)]
 #![allow(arithmetic_overflow)]
-#![allow(internal_features)]
+#![allow(internal_features, redundant_transmutation)]
 
 use std::any::type_name;
 use std::cmp::min;
